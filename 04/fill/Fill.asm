0b88cf2d844c6ee017ea06cf6d59f74921258b5a
wangyuxin <369264448@qq.com>
2021-10-14 16:40:08 +0800

    04

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+  @24575
+  D=A
+
+  @0
+  M=D
+
+  @SCREEN
+  D=A
+  @1
+  M=D
+(LOOP)
+  @KBD
+  D=M
+  @FILL
+  D;JGT
+
+  @CLEAR
+  0;JMP
+(FILL)
+  @0
+  D=M
+  @1
+  D=D-M
+  @LOOP
+  D;JLT
+
+  @1
+  D=M
+  A=M
+  M=-1
+
+  @1
+  M=D+1
+
+  @LOOP
+  0;JMP
+(CLEAR)
+  @SCREEN
+  D=A
+  @1
+  D=D-M
+  @LOOP
+  D;JGT
+
+  @1
+  D=M
+  A=M
+  M=0
+
+  @1
+  M=D-1
+
+  @LOOP
+  0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @2
+  M=0
+
+  @0
+  D=M
+  @END
+  D;JLT
+(LOOP)
+  @1
+  M=M-1
+  D=M
+  @END
+  D;JLT
+
+  @0
+  D=M
+  @2
+  M=M+D
+
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
